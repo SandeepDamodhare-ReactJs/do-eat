a2335e0f8364825226179e87db7d66fbcc0c0031
SANDEEPDAMODHARE <SDAMODHARE5@GMAIL.COM>
2024-06-07 14:44:50 +0530

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Mainroutes from './routing/Mainroutes'
 import NavBar from './components/NavBar'
-import Menu from './page/Menu'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [dbpath, setdbpath] = useState('http/localhost/loginsystem');
+ 
 
   return (
     <>
